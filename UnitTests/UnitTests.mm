f1ee5348f2eac44ec3df5c6f8d605819d596c04a
Dix Lorenz <dlorenz@medimach.de>
2018-10-09 10:44:46 +0200

    add UnitTests

---
+++ UnitTests/UnitTests.mm
@@ -0,0 +1,36 @@
+//
+//  UnitTests.m
+//  UnitTests
+//
+//  Created by Dix Lorenz on 09.10.18.
+//  Copyright © 2018 Dix Lorenz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UnitTests : XCTestCase
+
+@end
+
+@implementation UnitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+   XCTAssert(true);
+}
+
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+
+@end
